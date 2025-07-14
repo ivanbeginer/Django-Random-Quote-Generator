9f823e6d0e48bc46923d14b8afba8dc126571cc2
ivanbeginer <vaniaiv468@gmail.com>
2025-07-15 00:01:02 +0300

    viewer admin

--- viewer/admin.py
+++ viewer/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from viewer.models import Viewer
+
 # Register your models here.
+class ViewerAdmin(admin.ModelAdmin):
+    list_display = ['ip_address','watched_quotes','liked_quotes','disliked_quotes']
+
+
+
+admin.site.register(Viewer,ViewerAdmin)
